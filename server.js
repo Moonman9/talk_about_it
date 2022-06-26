03c1151552ffc2faf454c90aeee2d51e29b73554
Moonman9 <zzepold97@gmail.com>
2022-06-26 09:54:50 -0700

    connection configure setup

--- server.js
+++ server.js
@@ -23,3 +23,16 @@ const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(routes);
+sequelize.sync();
+app.listen(PORT, () => {
+    console.log(`App now listening on port ${PORT}`);
+});
